1280a36fbf1beaf96331c514697228e9ab73029f
Ruhong <seahrh@gmail.com>
2017-02-16 10:05:48 +0800

    After removing large file, add back: Test Search Name api - Multiple terms in query

--- src/test/java/sbac/api/NamesSearchResourceIT.java
+++ src/test/java/sbac/api/NamesSearchResourceIT.java
@@ -41,6 +41,23 @@ public class NamesSearchResourceIT extends ApiTestBase {
 	public Object[][] namesThatExistData() {
 		return new Object[][] { { "laura" }, { "mary" }, { "johnny" } };
 	}
+	
+	@Test
+	public void multipleTerms() {
+		// Query string contains multiple terms
+		// and there is at least 1 item in result set.
+		final String query = "linda 2003";
+		final String path = concat(ENDPOINT, query);
+		final Response rsp = API.path(path)
+			.request()
+			.get();
+		assertEquals(rsp.getStatus(), HttpServletResponse.SC_OK);
+		assertEquals(rsp.getMediaType()
+			.toString(), MediaType.APPLICATION_JSON);
+		final String json = rsp.readEntity(String.class);
+		List<Name> names = Name.fromJson(json);
+		assertEquals(names.isEmpty(), false);
+	}
 
 	@Test
 	public void emptyQueryString() {
